a3857b599157749537a7e04dca81e6c707b334dc
kaaku <92543349+sankaaku@users.noreply.github.com>
2021-10-17 02:04:55 -0700

    Add files via upload

---
+++ Cache Clear.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using System;
+using System.IO;
+
+[assembly: MelonInfo(typeof(CacheClear.Main), "Cache Clear", "1", "kaaku")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
+namespace CacheClear
+{
+    class Main : MelonPlugin
+    {
+        public override void OnApplicationEarlyStart()
+        {
+            Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low\\VRChat\\VRChat\\Cache-WindowsPlayer", true);
+            MelonLogger.Msg(ConsoleColor.Yellow, "Cache was cleared");
+        }
+    }
+}
